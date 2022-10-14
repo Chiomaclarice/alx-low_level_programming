3522db0411e10fdccc2031f0088254b4e27bfc0e
clarice <iclarice42@gmail.com>
2022-10-13 20:01:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+/**
+  *main-entry point of program
+  *
+  *Return:0 if no errors, otherwise non value zeros
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
 
