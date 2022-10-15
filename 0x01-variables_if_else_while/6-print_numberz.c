b8b60577de1e03094070aaf4a66076c2e913a955
clarice <iclarice42@gmail.com>
2022-10-15 14:18:35 -0700

    program prints 1-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main-entry point of program
+  *Return:0 if no error,non zero-value if errors
+**/
+
+int main(void)
+{	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar (c);
+	}
+		putchar ('\n');
+		return (0);
+}
