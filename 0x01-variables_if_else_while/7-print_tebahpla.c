46e34c1c518860ff2665db9025f0298bf3b8fc2f
clarice <iclarice42@gmail.com>
2022-10-15 14:36:55 -0700

    program prints alpahabet in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  *main program point
+  *Return:0 if no error, non zero value if no error
+**/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar (c);
+	}
+		putchar ('\n');
+		return (0);
+}
