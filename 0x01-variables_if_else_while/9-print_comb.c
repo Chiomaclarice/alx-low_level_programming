7ec3d1d084ad4a1a284d4da35efaea298a1fa0d2
clarice <iclarice42@gmail.com>
2022-10-15 15:20:03 -0700

    program prints possible combination digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  *main-program entry point
+  *Return:0 if no errors, non zero value if error
+**/
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+		if (n == '9')
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+		}
+	}
+			putchar('\n');
+			return (0);
+}
