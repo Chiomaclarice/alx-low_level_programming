3ef54888cf5b80b3561a670c531083ee02c343b0
clarice <iclarice42@gmail.com>
2022-10-18 16:28:16 -0700

    Program prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,15 @@
 
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	sz = sizeof(str) / sizeof(int);
-
-		for (count = 0; count < sz; count++)
-		{
-			_putchar('\n');
-		}
 
 	_putchar('\n');
 	return (0);
