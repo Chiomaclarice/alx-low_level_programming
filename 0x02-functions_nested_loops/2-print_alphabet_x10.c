4a36e41a8db154540270724236431de1365a5e6f
clarice <iclarice42@gmail.com>
2022-10-19 03:46:22 -0700

    function prints alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,15 @@
 
 	void print_alphabet_x10(void)
 	{
-	int j, count;
+	char j, k;
 
-	count = 0;
-
-	while (count <= 9)
+	for (k = 0; k < 10; k++)
 	{
 
 	for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar('j');
 	}
-	count++;
 		_putchar('\n');
 	}
 }
