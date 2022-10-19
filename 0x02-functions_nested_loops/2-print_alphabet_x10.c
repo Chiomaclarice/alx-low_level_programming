6642869019e7bafe2f62efd29ceea2cb2d0114c4
clarice <iclarice42@gmail.com>
2022-10-18 17:18:09 -0700

    function prints all lower case alpabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - prints all lowercase alphabet ten times
+  *Return:On success 1.
+*/
+
+void print_alphabet_x10(void);
+{
+char j;
+
+	for (j = 'a'; j <= 'z', j++)
+	for (j = 0; j <= 9; j++)
+	{
+		_putchar('j');
+		_putchar('\n');
+	}
