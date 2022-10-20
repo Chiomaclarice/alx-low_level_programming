cf3a5c8b170eaa2a5adb87d13c84e1d68b070ed2
clarice <iclarice42@gmail.com>
2022-10-19 18:56:30 -0700

    9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,6 @@ void times_table(void)
 
 		_putchar(44);
 		_putchar(32);
-		}
 
 		if (multi <= 9)
 		{
@@ -34,6 +33,7 @@ void times_table(void)
 			{
 			_putchar((multi / 10) + 48);
 			_putchar((multi % 10) + 48);
+			}
 		}
 			_putchar('\n');
 }
