e47ec789559ec58203e73f2d55f6091eb12228ff
clarice <iclarice42@gmail.com>
2022-10-19 19:12:55 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *add -function adds two integer
+  *@m: first number to compute
+  *@n: second number to compute
+  *Return: result
+*/
+
+int add(int m, int n)
+{
+	int result;
+
+	result = m + n;
+
+	return (result);
+}
