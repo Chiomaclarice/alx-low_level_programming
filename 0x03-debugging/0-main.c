e34c7dcaf51d81d50c636421344699ff17bec2c5
clarice <iclarice42@gmail.com>
2022-10-20 10:34:11 -0700

    main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *main- tests function that prints a negative or positive integer
+  *Return: 0
+*/
+
+
+int main(void)
+{
+	int j;
+
+	j = 0;
+
+	positive_or_negative(j);
+	return (0);
+}
