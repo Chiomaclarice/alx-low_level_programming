d347e0d8024d08e0daeac0e2f312d4e67ee06dc6
clarice <iclarice42@gmail.com>
2022-10-22 14:17:25 -0700

    function prints numbers from 0-14 x10

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,17 @@
 */
 
 void more_numbers(void)
-	{
-		int a, b;
+{
+	int i, ch;
 
-		for (a = 0; a <= 10; a++)
+	for (i = 0; i < 10; i++)
 	{
-
-		for (b = 0; b <= 14; b++)
+		for (ch = 0; ch < 15; ch++)
 		{
-		if (b >= 10)
-
-		_putchar((b / 10) + 48);
-		_putchar((b % 10) + 48);
-	}
-
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+					_putchar((ch % 10) + 48);
+		}
 		_putchar('\n');
 	}
-	}
+}
