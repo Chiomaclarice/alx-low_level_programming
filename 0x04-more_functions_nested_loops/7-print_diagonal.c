39180b0a266cbacec2a911ebfd0af94617defa13
clarice <iclarice42@gmail.com>
2022-10-22 08:24:54 -0700

    function prints diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,17 +11,15 @@ void print_diagonal(int n)
 
 	for (i = 0; i < n; i++)
 	{
-	for (j = 0; j < i; j++)
-	{
-		_putchar(' ');
-	}
-
-		_putchar(92);
-		if (i < (n - 1))
+		for (j = 0; j < i; j++)
 		{
-		_putchar('\n');
+			_putchar(' ');
+		}
+		_putchar(92);
+
+		if (i < (n - 1))
+			_putchar('\n');
 	}
 
 		_putchar('\n');
-}
-}
+	}
