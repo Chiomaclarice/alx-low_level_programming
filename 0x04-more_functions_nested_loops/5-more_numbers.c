d8a4096989d65ee9caf4964a9b3cc2845df5b4ef
clarice <iclarice42@gmail.com>
2022-10-22 13:50:17 -0700

    function prints numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,21 +6,20 @@
 */
 
 void more_numbers(void)
-{
-	int a, b;
+	{
+		int a, b;
 
 		for (a = 0; a <= 10; a++)
 	{
 
 		for (b = 0; b <= 14; b++)
-	{
-
+		{
 		if (b >= 10)
 
 		_putchar((b / 10) + 48);
 		_putchar((b % 10) + 48);
 	}
 
-	_putchar('\n');
-}
-}
+		_putchar('\n');
+	}
+	}
