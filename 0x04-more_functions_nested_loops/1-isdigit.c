a20786cb354d5e3465444cf7ab1121736813e92e
clarice <iclarice42@gmail.com>
2022-10-22 04:54:09 -0700

    function prints numbers 0-9

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
   *_isdigit- checks if parameter is a digit through 0 - 9
-  *@c: input  number
-  *Return: 1 if is a number(0-9) otherwise 0
+  *@c: input number
+  *Return: 1 if is a number(0 to 9) otherwise 0
 */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 	{
 		return (1);
 	}
