4995e738fb9d6acc1ea5f61e69888ef70d123e94
clarice <iclarice42@gmail.com>
2022-11-02 16:09:26 -0700

    function copies memory areaa from src to dest

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_memcpy - function copies src to dest
+  *@src: source memory
+  *@dest: destination memory
+  *@n:no of bytes
+  *Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	int i;
+	char *ptr = dest;
+
+	for (i = 0; i < n; i++)
+		dest[i] == src[i]
+			return (dest);
+}
