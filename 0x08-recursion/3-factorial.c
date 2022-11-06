3fece0513880ab9b1a6ce5b858c5ea0f57238dcc
clarice <iclarice42@gmail.com>
2022-11-05 17:06:30 -0700

    function prints factorial of a give number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,16 +2,19 @@
 /**
   *_factorial - function returns the factorial of a given number
   *@n: given number
-  *Return: -1 if n is less than 0, 1 if it is equal to 0
-  *else return factorial n.
+  *Return: factorial of given number
 */
 
 int factorial(int n);
 {
 	if (n < 0)
+	{
 		return (-1);
+	}
 	else if (n == 0)
+	{
 		return (1);
+	}
 	else
 		return (n * factorial(n - 1));
 }
