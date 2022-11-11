5c7bd5ee34d184ff53ae954f056e7cffde7fc582
clarice <iclarice42@gmail.com>
2022-11-11 13:43:39 -0800

    concatenates

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
+#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "holberton.h"
 
 /**
   * *str_concat - concatenates two strings
@@ -11,44 +11,33 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	char *s3;
+	char *result;
 
-	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	unsigned int i, j, len1, len2, len;
 
-	while (s1 && s1[len1])
-	len1++;
-	while (s2 && s2[len2])
-	len2++;
+	len1 = len2 = 0;
 
-	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (s3 == NULL)
-	return (NULL);
-
+	if (s1 != NULL)
+	{
 	i = 0;
-	j = 0;
-
-	if (s1)
-	{
-	while (i < len1)
-	{
-	s3[i] = s1[i];
-	i++;
+	while (s1[i++] != '\0')
+	len1++;
 	}
-	}
-
-	if (s2)
+	if (s2 != NULL)
 	{
-	while (i < (len1 + len2))
-	{
-	s3[i] = s2[j];
-	i++;
-	j++;
-
+	i = 0;
+	while (s2[i++] != '\0')
+	len2++;
 	}
-	}
-	s3[i] = '\0';
-	return (s3);
-}
 
-
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+	return (NULL);
+	for (i = 0; i < len1; i++)
+	result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+	result[i] = s2[j];
+	result[len] = '\0';
+	return (result);
 }
